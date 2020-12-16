b299d98d92fd07a2655a6ca5d264221ab3a884e9
Justas Titovec <justas.titovec@yahoo.com>
2020-12-16 14:17:10 +0100

    added Search component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Accordion from './components/Accordion';
+import Search from './components/Search';
 
 const items = [
   {
@@ -20,7 +21,8 @@ function App() {
   return (
     <div>
       <br />
-      <Accordion items={items} />
+      {/* <Accordion items={items} /> */}
+      <Search />
     </div>
   );
 }
