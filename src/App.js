87dba1dcb62f79a8d53c99e2f078970b4ef5b113
Justas Titovec <justas.titovec@yahoo.com>
2020-12-16 19:05:13 +0100

    dropdown

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+import React, { useState } from 'react';
 
 const items = [
   {
@@ -17,12 +19,34 @@ const items = [
   },
 ];
 
+const options = [
+  {
+    label: 'Color Red',
+    value: 'red',
+  },
+  {
+    label: 'Color blue',
+    value: 'blue',
+  },
+  {
+    label: 'Color green',
+    value: 'green',
+  },
+];
+
 function App() {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
       <br />
       {/* <Accordion items={items} /> */}
-      <Search />
+      {/* <Search /> */}
+      <Dropdown
+        onSelectedChange={setSelected}
+        selected={selected}
+        options={options}
+      />
     </div>
   );
 }
